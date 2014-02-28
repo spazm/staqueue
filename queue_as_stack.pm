fcb571c5fa5ad641e00518ebda167c2040698fec
Andrew Grangaard <granny-github@ofb.net>
2014-02-28 22:01:53 +0000

    add minimum value tracking.

--- queue_as_stack.pm
+++ queue_as_stack.pm
@@ -1,5 +1,7 @@
 package queue_as_stack;
 
+use List::Util qw(min);
+
 =pod
 
 =head1 NAME
@@ -23,6 +25,8 @@ Items are popped off of out_stack as they are removed from the queue.
 
 If out_stack is empty, all items are popped from in_stack and pushed onto out_stack
 
+requires O(m) space for storing the two queues.  2m of temporary space is required for the two stacks.
+
 =cut
 
 use Moo;
@@ -34,13 +38,78 @@ has in_stack => (
 );
 
 has out_stack => (
-    is      => 'ro',
-    default => sub { [] },
+    is        => 'ro',
+    predicate => 1,
+    default   => sub { [] },
 );
+
+has _in_stack_min => (
+    is        => 'rwp',
+    lazy      => 1,
+    builder   => 1,
+    predicate => 1,
+    clearer   => 1,
+);
+
+has _out_stack_min => (
+    is        => 'rwp',
+    lazy      => 1,
+    builder   => 1,
+    predicate => 1,
+    clearer   => 1,
+);
+
 
 =head1 METHODS
 
 =over 8
+
+=item _build__out_stack_min
+
+Lazy builder to calculate the minimum value in the output stack.
+
+The queue maintains the smallest value on insert.  When a minimum value is dequeued, the object must calculate the current smallest value.
+
+=cut
+
+sub _build__out_stack_min
+{
+    my $self = shift;
+
+    return min(@{$self->out_stack});
+}
+
+sub _build__in_stack_min
+{
+    return undef;
+}
+
+=item getMinimum()
+
+returns the value of the smallest numeric value in the queue.  Only appropriate when storing numeric values.
+
+returns undef if there are no values in the queue.
+
+=cut
+
+sub getMinimum
+{
+    my $self = shift;
+    my $in = $self->_in_stack_min;
+    my $out = $self->_out_stack_min;
+    if (defined $in and defined $out)
+    {
+        return min($in, $out);
+    }
+    elsif( defined $in )
+    {
+        return $in;
+    }
+    else
+    {
+        return $out;
+    }
+}
 
 =item enqeue(value)
 
@@ -51,7 +120,17 @@ push value(s) onto the queue.  Accepts an array and will push values into the qu
 sub enqueue
 {
     my $self = shift;
-    push @{ $self->in_stack }, $_ for @_;
+    for my $value (@_)
+    {
+        #maintain the min
+        if ( !defined( $self->_in_stack_min )
+            or $value < $self->_in_stack_min )
+        {
+            $self->_set__in_stack_min($value);
+        }
+
+        push @{ $self->in_stack }, $value;
+    }
 }
 
 =item dequeue()
@@ -61,18 +140,34 @@ pop a single value from the queue
 =cut
 
 sub dequeue
-{
-    my $self = shift;
-    if ( !@{ $self->out_stack } )
     {
-
-        # copy all items from in_stack to out_stack
-        while ( @{ $self->in_stack } )
+        my $self = shift;
+        if ( !@{ $self->out_stack } )
         {
-            push( @{ $self->out_stack }, pop @{ $self->in_stack } );
+
+            # move items from in_stack to out_stack, can copy over min.
+            $self->_set__out_stack_min( $self->_in_stack_min );
+            $self->_clear_in_stack_min;
+
+            while ( @{ $self->in_stack } )
+            {
+
+                # copy in_stack minimum to out_stack here
+                push( @{ $self->out_stack }, pop @{ $self->in_stack } );
+            }
         }
-    }
-    return pop @{ $self->out_stack };
+
+        return undef if !@{ $self->out_stack };
+
+        my $return = pop @{ $self->out_stack };
+
+        if ( $self->_has_out_stack_min && $return <= $self->_out_stack_min )
+        {
+            print STDERR "popping [$return] and clearing out_stack_min\n";
+            $self->_clear_out_stack_min;
+        }
+
+        return $return;
 }
 
 =back
--- queue_as_stack.t
+++ queue_as_stack.t
@@ -3,12 +3,16 @@ use Test::More;
 use_ok('queue_as_stack');
 
 my $q = queue_as_stack->new();
+is( $q->getMinimum, undef, 'undef getMinimum when empty');
 is( $q->dequeue(), undef, 'undef when dequeue-ing an empty queue' );
 
 $q->enqueue(1);
+is( $q->getMinimum, 1, 'getMinimum for single entry');
 is( $q->dequeue(), 1, 'enqueue and dequeue a single value' );
+is( $q->getMinimum, undef, 'getMinimum undef after emptying a single item queue');
 
 $q->enqueue($_) for ( 1, 2, 3, 4 );
+is( $q->getMinimum, 1, 'getMinimum for multiple entries in in_stack');
 
 #check internals
 is_deeply( $q->in_stack, [ 1, 2, 3, 4 ], 'all items enqueued to in_stack' );
@@ -18,8 +22,11 @@ is_deeply(
     [ 4, 3, 2 ],
     'all items enqueued into out_stack in reverse order'
 );
+is( $q->getMinimum, 2, 'getMinimum for 2');
 is( $q->dequeue, 2, 'second item dequeued from queue' );
 is( $q->dequeue, 3, 'third item dequeued from queue' );
+is( $q->getMinimum, 4, 'getMinimum for last entry');
 is( $q->dequeue, 4, 'fourth item dequeued from queue' );
+is( $q->getMinimum, undef, 'getMinimum is undef for empty queue');
 
 done_testing;
